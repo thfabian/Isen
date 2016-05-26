6b0c9d338c7d40de4d5f8bdb987954c71856d5d3
thfabian <fabian_thuering@hotmail.com>
2016-05-27 01:05:48 +0200

    Adding Visualizer.py skeleton

--- python/IsenPython/Visualizer.py
+++ python/IsenPython/Visualizer.py
@@ -6,8 +6,8 @@
 #                     _/ / ___/ / /___/ /|  /  
 #                    /___//____/_____/_/ |_/   
 #            
-#  Isentropic model - ETH ZÃ¼rich
-#  Copyright (C) 2016  Fabian ThÃ¼ring (thfabian@student.ethz.ch)
+#  Isentropic model - ETH Zürich
+#  Copyright (C) 2016  Fabian Thüring (thfabian@student.ethz.ch)
 # 
 #  This file is distributed under the MIT Open Source License. See LICENSE.TXT for details.
 #
@@ -15,5 +15,17 @@
 
 from __future__ import print_function, division
 
-from .IsenPythonCxx import *
-from .Visualizer import Visualizer
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import argparse
+
+class Visualizer:
+    """ Visualize output of simulation """
+    
+    def __init__(self):
+        """ Initalize vizualizer methods """
+        print("Visualizer::init")
+
+if __name__ == '__main__':
+    pass
--- python/test.py
+++ python/test.py
@@ -3,6 +3,8 @@ from __future__ import print_function, division
 import numpy as np
 import IsenPython
 
+visualiter = IsenPython.Visualizer()
+
 solver = IsenPython.Solver()
 solver.init()
 solver.run()
